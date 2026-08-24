0f0bc7e1d14fa5642e0120baf5c9a7cf69e2fbec
Tomcat <i@zxf.io>
2026-08-24 21:30:14 +0800

    fix: shorten Android launcher label

--- travel-android/app/build.gradle.kts
+++ travel-android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "io.zxf.flowsplice.travel"
         minSdk = 34
         targetSdk = 37
-        versionCode = 6
+        versionCode = 7
         versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
